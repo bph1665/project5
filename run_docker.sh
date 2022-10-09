bc28ab1cd96bef372c4f30918120a4fb2a2c9aa5
Ben Herman <ben.herman@nestle.com>
2022-10-08 19:19:00 -0500

    Add docker build staeps to pipeline

--- run_docker.sh
+++ run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
- docker build -t bostonhousing .
+docker build -t project5:latest .
  
 # Step 2: 
 # List docker images
@@ -12,4 +12,4 @@ docker image ls
 
 # Step 3: 
 # Run flask app
-docker run -p 8000:80 bostonhousing:latest
+docker run -p 80:80 project5:latest
